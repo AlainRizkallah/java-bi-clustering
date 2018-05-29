f124d3abbd99d5f8245de4f20490d1535fa87cb3
Nicolas Kritter <nicolas.kritter@isep.fr>
2018-05-29 14:04:10 +0200

    get mean of submatrixIJ

--- src/main/MatrixUtils.java
+++ src/main/MatrixUtils.java
@@ -1,88 +1,108 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-
 public class MatrixUtils {
-	//mean on rows or a given column index
-	public static float meanByRow(List<List<Integer> > matrix, int index){
-		return getMean(getRowbyIndex(matrix,index));
+	// mean on rows or a given column index
+	public static float meanByRow(List<List<Integer>> matrix, int index) {
+		return getMean(getRowbyIndex(matrix, index));
 	}
-	//mean on columns or a given row index
-	public static float meanByColumn(List<List<Integer> > matrix, int index){
-		return getMean(getColbyIndex(matrix,index));
+
+	// mean on columns or a given row index
+	public static float meanByColumn(List<List<Integer>> matrix, int index) {
+		return getMean(getColbyIndex(matrix, index));
 	}
+
 	// mean of a sub-matrix
-	public static float meanOfSubMatrix(List<List<Integer> > matrix){
+	public static float meanOfSubMatrix(List<List<Integer>> matrix) {
 		int len = matrix.size();
 		float sum = 0;
 		for (List<Integer> ligne : matrix) {
-			sum+= getMean(ligne);
+			sum += getMean(ligne);
 		}
-		return sum/len;
+		return sum / len;
 	}
-	
-	public static void printArray(int[][]G) {
+
+	public static float meanOfSubMatrix(List<List<Integer>> I, List<List<Integer>> J) {
+		float sum = 0;
+		for (List<Integer> list : J) {
+			sum += getMean(list);
+		}
+		for (List<Integer> list : I) {
+			sum += getMean(list);
+		}
+		return sum/(I.size()*J.size());
+	}
+
+	public static void printArray(int[][] G) {
 		for (int i = 0; i < G.length; i++) {
 			System.out.println(Arrays.toString(G[i]));
 		}
 
 	}
-	public static void printArraylist(List<List<Integer>>G) {
-		for (List<Integer>g : G) {
+
+	public static void printArraylist(List<List<Integer>> G) {
+		for (List<Integer> g : G) {
 			System.out.println(g);
 		}
 
 	}
 
-	public static List<Integer> getRowbyIndex(List<List<Integer>> matrix, int index){
+	public static List<Integer> getRowbyIndex(List<List<Integer>> matrix,
+			int index) {
 		return matrix.get(index);
 	}
 
-	public static List<Integer> getColbyIndex(List<List<Integer>> matrix, int index){
-		List<Integer> col=  new ArrayList<Integer>();
-		for (List<Integer> row:matrix) {
+	public static List<Integer> getColbyIndex(List<List<Integer>> matrix,
+			int index) {
+		List<Integer> col = new ArrayList<Integer>();
+		for (List<Integer> row : matrix) {
 			col.add(row.get(index));
 		}
 		return col;
 	}
+
 	public static void removeColbyIndex(List<List<Integer>> matrix, int index) {
-		for(int i=0;i<matrix.size();i++)
-		matrix.get(i).remove(index);
+		for (int i = 0; i < matrix.size(); i++)
+			matrix.get(i).remove(index);
 	}
-	
-	
+
 	public static float getMean(List<Integer> array) {
-		if (array==null) return 0;
+		if (array == null)
+			return 0;
 		int len = array.size();
-		if (len==0) return 0;
+		if (len == 0)
+			return 0;
 		float sum = 0;
-		for (int e:array) {
-			sum+=e;
+		for (int e : array) {
+			sum += e;
 		}
-		return sum/len;
+		return sum / len;
 	}
+
 	public static float getMean(int[] array) {
-		int len =array.length;
-		if (len==0) return 0;
+		int len = array.length;
+		if (len == 0)
+			return 0;
 		float sum = 0;
-		for (int e:array) {
-			sum+=e;
+		for (int e : array) {
+			sum += e;
 		}
-		return sum/len;
+		return sum / len;
 	}
-	
+
 	public static List<List<Integer>> twoDArrayToListList(int[][] twoDArray) {
 		List<List<Integer>> list = new ArrayList<>();
-	    for (int[] array : twoDArray) {
-	    	List<Integer>sublist= new ArrayList<>();
-	        for(int x : array)
-	        	sublist.add(x);
-	        list.add(sublist);
-	    }
-	    return list;
+		for (int[] array : twoDArray) {
+			List<Integer> sublist = new ArrayList<>();
+			for (int x : array)
+				sublist.add(x);
+			list.add(sublist);
+		}
+		return list;
 	}
 
 }
-
